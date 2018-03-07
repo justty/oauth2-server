b8f3841cd1752f29283a15bf8836606437db5161
Aliaksei Varabei <avarabei@users.noreply.github.com>
2018-03-07 21:38:18 +0300

    Update BearerTokenResponse.php

--- src/ResponseTypes/BearerTokenResponse.php
+++ src/ResponseTypes/BearerTokenResponse.php
@@ -39,10 +39,7 @@ class BearerTokenResponse extends AbstractResponseType
                         'client_id'        => $this->accessToken->getClient()->getIdentifier(),
                         'refresh_token_id' => $this->refreshToken->getIdentifier(),
                         'access_token_id'  => $this->accessToken->getIdentifier(),
-
-                        // todo: hardcode
-                        //'scopes'           => $this->accessToken->getScopes(),
-
+                        'scopes'           => $this->accessToken->getScopes(),
                         'user_id'          => $this->accessToken->getUserIdentifier(),
                         'expire_time'      => $this->refreshToken->getExpiryDateTime()->getTimestamp(),
                     ]
